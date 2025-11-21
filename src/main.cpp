b546df6a3988c9ad4dce866b9333dee3f47e0928
GargEngineers7002 <officialchiraggarg7002@gmail.com>
2025-11-21 23:17:33 +0530

    type command

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <string>
+#include <unordered_set>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::unordered_set<std::string> builtins;
+  builtins.insert("echo");
+  builtins.insert("type");
+  builtins.insert("exit");
   // TODO: Uncomment the code below to pass the first stage
   while (true) {
     std::cout << "$ ";
@@ -14,10 +19,18 @@ int main() {
     if (input == "exit") {
       break;
     }
+
     size_t space_pos = input.find(' ');
     std::string first_word = input.substr(0, space_pos);
+
     if (first_word == "echo") {
       std::cout << input.substr(space_pos + 1) << std::endl;
+    } else if (first_word == "type") {
+      if (builtins.count(input.substr(5))) {
+        std::cout << input.substr(5) << "is a shell builtin" << std::endl;
+      } else {
+        std::cout << input.substr(5) << ": not found" << std::endl;
+      }
     } else {
       std::cout << input << ": command not found" << std::endl;
     }

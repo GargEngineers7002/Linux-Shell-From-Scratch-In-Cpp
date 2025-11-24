9e8cbef2dd51b1dc5d5c1acee20a78d6cf3a6a25
GargEngineers7002 <officialchiraggarg7002@gmail.com>
2025-11-24 21:27:25 +0530

    PATH

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,88 @@
+#include <cstdlib>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <unistd.h>
 #include <unordered_set>
+#include <vector>
 
-int main() {
+std::unordered_set<std::string> builtins = {"echo", "type", "exit"};
+
+std::string check_PATH(std::string command)
+{
+  const char *path_env = std::getenv("PATH");
+  if (!path_env)
+    return "";
+
+  std::string s(path_env);
+  std::stringstream ss(s);
+  std::string segment;
+
+  while (std::getline(ss, segment, ':'))
+  {
+    std::string full_path = segment + '/' + command;
+    if (access(full_path.c_str(), X_OK) == 0)
+    {
+      return full_path;
+    }
+  }
+  return "";
+}
+
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::unordered_set<std::string> builtins;
-  builtins.insert("echo");
-  builtins.insert("type");
-  builtins.insert("exit");
   // TODO: Uncomment the code below to pass the first stage
-  while (true) {
+  while (true)
+  {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
-    if (input == "exit") {
+    if (input == "exit")
+    {
       break;
     }
 
-    size_t space_pos = input.find(' ');
-    std::string first_word = input.substr(0, space_pos);
+    std::stringstream ss(input);
+    std::string command;
+    ss >> command;
 
-    if (first_word == "echo") {
-      std::cout << input.substr(space_pos + 1) << std::endl;
-    } else if (first_word == "type") {
-      if (builtins.count(input.substr(5))) {
-        std::cout << input.substr(5) << " is a shell builtin" << std::endl;
-      } else {
-        std::cout << input.substr(5) << ": not found" << std::endl;
+    std::string argument;
+    std::getline(ss, argument);
+
+    if (!argument.empty() && argument[0] == ' ')
+    {
+      argument = argument.substr(1);
+    }
+
+    if (command == "echo")
+    {
+      std::cout << argument << std::endl;
+    }
+    else if (command == "type")
+    {
+      if (builtins.count(argument))
+      {
+        std::cout << argument << " is a shell builtin" << std::endl;
       }
-    } else {
+      else
+      {
+        std::string path = check_PATH(argument);
+        if (!path.empty())
+        {
+          std::cout << argument << " is " << path << std::endl;
+        }
+        else
+        {
+          std::cout << argument << ": not found" << std::endl;
+        }
+      }
+    }
+    else
+    {
       std::cout << input << ": command not found" << std::endl;
     }
   }
